929972bd267e012e6c7e01f70c443b2dc072616d
忘忧北萱草 <1779724477@qq.com>
2018-04-28 22:45:57 +0800

    big change

--- script/article.js
+++ script/article.js
@@ -1,3 +1,26 @@
+function onSchSubmit(){
+    let url = encodeURI($(".sch").val());
+    if (url != ""){
+      // window.open("https://www.baidu.com/#ie=UTF-8&&wd=" + url);
+      url = "https://m.baidu.com/#ie=UTF-8&&wd=" + url;
+      $("#subform").attr("src", url).slideDown();
+      $("#subform a").attr("src", url);
+    }else{
+      $("#subform").attr("src", "").slideUp();
+    }
+  }
 $(function(){
+  "use strict";
+  let slidebar = $("#slidebar");
+  slidebar.slideUp(0);
+  $("#menu").click(function(){
+    slidebar.slideToggle();
+  });
+  $("#main").click(function(){
+    slidebar.slideUp();
+    $("#subform").attr("src", "").slideUp();
+  });
+  $("#subform").slideUp(0);
+  $(".schbtn").click(onSchSubmit);
   $("pre.hightlight").removeClass("hightlight");
 });
