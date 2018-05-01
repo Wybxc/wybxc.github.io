5cb9369daf0d94668d7aecbf284a7ab638b62320
忘忧北萱草 <1779724477@qq.com>
2018-05-01 09:21:39 +0800

    another big change 7 & many bling

--- script/article.js
+++ script/article.js
@@ -1,3 +1,4 @@
+const bcolor = str => "rgba(187,152,178," + str + ")";
 function returnTop(){
   $("html,body").animate({scrollTop: 0}, Math.log(document.documentElement.scrollTop) * 100);
 }
@@ -18,14 +19,36 @@ function onSchSubmit(){
 }
 $(function(){
   "use strict";
+  $("div.tagform div").css('background-color', bcolor(1));
+  $("div.slidebar").css('border-color', bcolor(1));
   // 边栏 Expand
+  let b = true;
   let slidebar = $("#slidebar");
   slidebar.slideUp(0);
   $("#menu").click(function(){
-    slidebar.slideToggle();
+    if (b || (document.documentElement.scrollTop > 50)){
+      $("#nav").stop().animate({backgroundColor: bcolor(1)}, 500); 
+      $("#main").stop().animate({marginLeft: "20%"}, 500);
+      slidebar.stop().animate({opacity: 1}, 200);
+    } else {
+      $("#nav").stop().animate({backgroundColor: bcolor(0)}, 500);
+      $("#main").stop().animate({marginLeft: "10%"}, 500);
+      slidebar.stop().animate({opacity: 0}, 200);
+    }
+    b = !b;
+    slidebar.slideToggle(500);
   });
   $("#main").click(function(){
-    slidebar.slideUp();
+    b = true;
+    slidebar.slideUp(500);
+    if (document.documentElement.scrollTop > 50) {
+      $("#nav").stop().animate({backgroundColor: bcolor(1)}, 500);
+      slidebar.stop().animate({opacity: 1},200);
+    } else {
+      $("#nav").stop().animate({backgroundColor: bcolor(0)}, 500);
+      slidebar.stop().animate({opacity: 0},200);
+    }
+    $("#main").stop().animate({marginLeft: "10%"},500);
     $("#subform").attr("src", "").slideUp();
   });
   $("#subform").slideUp(0);
@@ -33,12 +56,10 @@ $(function(){
   $(".schbtn").click(onSchSubmit);
   // 导航栏变色
   onscroll = function(){
-    if (document.documentElement.scrollTop > 50) {
-      $("#nav").stop().animate({backgroundColor: "rgba(187,152,178,1)"}, 300);
-      $("#slidebar").stop().animate({backgroundColor: "rgba(187,152,178,0.5)"}, 300);
+     if (!b || (document.documentElement.scrollTop > 50)) {
+      $("#nav").stop().animate({backgroundColor: bcolor(1)}, 300);
     } else {
-      $("#nav").stop().animate({backgroundColor: "rgba(128,128,128,0)"}, 300);
-      $("#slidebar").stop().animate({backgroundColor: "rgba(187,152,178,0)"}, 300);
+      $("#nav").stop().animate({backgroundColor: bcolor(0)}, 300);
     }
   };
   onscroll();
@@ -48,21 +69,21 @@ $(function(){
     const a = Number(index) - Number($('li[index]').first().attr('index'));
     $("div[index=" + index + "]").stop().css("top", (a * 52 + 65).toString() + "px").show();
     $("#tagform").stop().show();
-    $(this).css('background-color', "rgba(187,152,178,0.8");
+    $(this).css('background-color', bcolor(0.8));
   });
   $("div[index]").mouseenter(function(){
     $(this).stop().show();
     $("#tagform").stop().show();
-    $("li[index=" + $(this).attr("index") + "]").css('background-color', "rgba(187,152,178,0.8");
+    $("li[index=" + $(this).attr("index") + "]").css('background-color', bcolor(0.8));
   });
   $("li[index]").mouseleave(function(){
     $("div[index=" + $(this).attr("index") + "]").hide(1);
     $("#tagform").hide(1);
-    $(this).css('background-color', "rgba(187,152,178,0");
+    $(this).css('background-color', bcolor(0));
   });
   $("div[index]").mouseleave(function(){
     $(this).hide(1); // <- 黑科技
     $("#tagform").hide(1);
-    $("li[index=" + $(this).attr("index") + "]").css('background-color', "rgba(187,152,178,0");
+    $("li[index=" + $(this).attr("index") + "]").css('background-color', bcolor(0));
   });
 });
