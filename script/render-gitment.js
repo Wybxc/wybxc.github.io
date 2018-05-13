ea7d482f15998f566f865762a03a73b1134416ae
忘忧北萱草 <1779724477@qq.com>
2018-05-13 12:48:12 +0800

    Custom Gitment IX

--- script/render-gitment.js
+++ script/render-gitment.js
@@ -5,29 +5,7 @@ const wybxcTheme = ablePro ? {
     const container = document.createElement('div');
     container.className = 'gitment-container gitment-root-container';
     container.appendChild(instance.renderHeader(state, instance));
-    const comments = instance.renderComments(state, instance);
-    $(comments).find('[lang=en-US]').removeAttr('lang');
-    $(comments).find('.gitment-comment-header').each(function(){
-      const user = $(this).find('.gitment-comment-name').first();
-      const userName = user.text();
-      const userLink = user.attr('href');
-      user.remove();
-      const date = $(this).find('span[title]').first()
-      const time = new Date(date.attr('title'));
-      date.attr('title', time.toLocaleString());
-      date.text(`${time.getFullYear()}年${time.getMonth()+1}月${time.getDate()}日`);
-      $(this).text('').prepend(
-        '<span>评论于</span>'
-      ).prepend(
-        $('<a></a>').text(userName).attr({href:userLink, target:'_blank'}).addClass('gitment-comment-name')
-      );
-      const editDate = date.next().filter('span[title]');
-      editDate.each(function(){
-        const editTime = new Date($(this).attr('title'));
-        $(this).attr('title', editTime.toLocaleString());
-      });
-    });
-    container.appendChild(comments);
+    container.appendChild(instance.renderComments(state, instance));
     container.appendChild(instance.renderEditor(state, instance));
     container.appendChild(instance.renderFooter(state, instance));
     return container;
@@ -37,7 +15,7 @@ const wybxcTheme = ablePro ? {
     const meta = state.meta,
           user = state.user,
           reactions = state.reactions;
-    const dot = '\u2022';
+    const dot = '&nbsp;\u2022&nbsp;';
     // 容器
     const container = document.createElement('div');
     container.className = 'gitment-container gitment-header-container';
@@ -48,8 +26,6 @@ const wybxcTheme = ablePro ? {
     const likeButton = document.createElement('span');
     likeButton.className = 'gitment-header-like-btn';
     likeButton.innerHTML = (likedReaction ? '取消感谢' : '感谢');
-    (meta.reactions && meta.reactions.heart) ?
-      (likeButton.append(dot + `<strong>${meta.reactions.heart}</strong>人感谢过`)):void(0);
     $(likeButton).prepend('<span class="fa fa fa-heart-o"></span>');
     if (likedReaction) {
       likeButton.classList.add('liked');
@@ -59,6 +35,8 @@ const wybxcTheme = ablePro ? {
       likeButton.onclick = () => instance.like();
     }
     container.appendChild(likeButton);
+    (meta.reactions && meta.reactions.heart) ?
+      ($(container).append(dot + `<strong>${meta.reactions.heart}</strong>人感谢过`)):void(0);
     // Comments Count
     meta.comments ?
       ($(container).append(dot + `<span>${meta.comments}条评论</span>`)) : void(0);
@@ -97,4 +75,28 @@ const gitment = new Gitment({
   perPage: 10,
   theme: wybxcTheme
 });
-$(function(){gitment.render('comments');});
+$(function(){
+  gitment.render('comments');
+  const comments = $('.gitment-comments-container');
+  comments.find('[lang=en-US]').removeAttr('lang');
+  comments.find('.gitment-comment-header').each(function(){
+    const user = $(this).find('.gitment-comment-name').first();
+    const userName = user.text().trim();
+    const userLink = user.attr('href');
+    user.remove();
+    const date = $(this).find('span[title]').first()
+    const time = new Date(date.attr('title'));
+    date.attr('title', time.toLocaleString());
+    date.text(`${time.getFullYear()}年${time.getMonth()+1}月${time.getDate()}日`);
+    $(this).text('').prepend(
+      '<span>评论于</span>'
+    ).prepend(
+      $('<a></a>').text(userName).attr({href:userLink, target:'_blank'}).addClass('gitment-comment-name')
+    );
+    const editDate = date.next().filter('span[title]');
+    editDate.each(function(){
+      const editTime = new Date($(this).attr('title'));
+      $(this).attr('title', editTime.toLocaleString());
+    });
+  });
+});
