898851e7078bbb834f225af42b73ecdb9d4b8e27
忘忧北萱草 <1779724477@qq.com>
2018-05-14 22:50:56 +0800

    LoadScript.js

---
+++ script/loadscript.js
@@ -0,0 +1,3 @@
+const loadScript = (f, src) => $('body').ready(() => f() ? $.getScript(src) : null);
+// MathJax
+loadScript(() => /\$\$.*\$\$/.test($('#main').text()), 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/latest.js?config=TeX-AMS_HTML');
