b9978758ba65affb03d06acfd76a18b05690bbbf
Jiayi Zhuang <wybxc@qq.com>
2026-01-09 10:06:38 +0800

    feat: add rehype plugins for slugging and autolinking headings, update markdown processing

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,92 +4,114 @@ import { typst } from "astro-typst";
 import { visit } from "unist-util-visit";
 // @ts-expect-error
 import { rehypeTransformJsxInTypst } from "./node_modules/typstx/lib/core.js";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+
+/** @type {(script: string) => import("hast").Element} */
+const jsx = (script) => ({
+  type: "element",
+  tagName: "script",
+  properties: { "data-jsx": script },
+  children: [],
+});
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blog.wybxc.cc",
-	integrations: [
-		typst({
-			target: "html",
-			htmlMode: "hast",
-		}),
-	],
-	experimental: {
-		fonts: [
-			{
-				provider: fontProviders.fontsource(),
-				name: "Monaspace Neon",
-				cssVariable: "--font-monaspace-neon",
-				fallbacks: ["Georgia", "Times", "serif"],
-			},
-			{
-				provider: "local",
-				name: "MLModern",
-				cssVariable: "--font-mlmodern",
-				variants: [
-					{
-						weight: 400,
-						style: "normal",
-						src: ["./src/assets/fonts/mlmodern-regular.woff2"],
-					},
-					{
-						weight: 400,
-						style: "italic",
-						src: ["./src/assets/fonts/mlmodern-italic.woff2"],
-					},
-					{
-						weight: 700,
-						style: "normal",
-						src: ["./src/assets/fonts/mlmodern-bold.woff2"],
-					},
-				],
-				fallbacks: ["monaspace"],
-			},
-		],
-	},
-	image: {
-		remotePatterns: [
-			{
-				protocol: "data",
-			},
-		],
-	},
-	markdown: {
-		rehypePlugins: [
-			() => (tree) => {
-				// post process: remove the link inside citation
-				visit(tree, { tagName: "a" }, (node) => {
-					if (node.properties.role === "doc-biblioref") {
-						node.tagName = "span";
-						// make DOI links open in new tab
-						visit(node, { tagName: "a" }, (node) => {
-							node.properties.target = "_blank";
-						});
-					}
-				});
-			},
-			() => (tree) => {
-				visit(tree, { tagName: "body" }, (node) => {
-					node.children.splice(0, 0, {
-						type: "element",
-						tagName: "script",
-						properties: {
-							"data-jsx": `import { Image as AstroImage } from "astro:assets";`,
-						},
-						children: [],
-					});
-				});
-				visit(tree, { tagName: "img" }, (node) => {
-					if (node.properties.class === "typst-frame") {
-						const src = node.properties.src;
-						node.tagName = "script";
-						node.properties = {
-							"data-jsx": `<AstroImage src="${src}" alt="" format="svg" inferSize />`,
-						};
-					}
-				});
-			},
-			() => rehypeTransformJsxInTypst(),
-		],
-	},
+  site: "https://blog.wybxc.cc",
+  integrations: [
+    typst({
+      target: "html",
+      htmlMode: "hast",
+    }),
+  ],
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.fontsource(),
+        name: "Monaspace Neon",
+        cssVariable: "--font-monaspace-neon",
+        fallbacks: ["Georgia", "Times", "serif"],
+      },
+      {
+        provider: "local",
+        name: "MLModern",
+        cssVariable: "--font-mlmodern",
+        variants: [
+          {
+            weight: 400,
+            style: "normal",
+            src: ["./src/assets/fonts/mlmodern-regular.woff2"],
+          },
+          {
+            weight: 400,
+            style: "italic",
+            src: ["./src/assets/fonts/mlmodern-italic.woff2"],
+          },
+          {
+            weight: 700,
+            style: "normal",
+            src: ["./src/assets/fonts/mlmodern-bold.woff2"],
+          },
+        ],
+        fallbacks: ["monaspace"],
+      },
+    ],
+  },
+  image: {
+    remotePatterns: [
+      {
+        protocol: "data",
+      },
+    ],
+  },
+  markdown: {
+    rehypePlugins: [
+      () => (tree) => {
+        visit(tree, { tagName: "body" }, (node) => {
+          node.children.splice(
+            0,
+            0,
+            jsx(`import { Image as AstroImage } from "astro:assets";`),
+            jsx(`import { Link } from "@lucide/astro";`),
+          );
+        });
+      },
+      () => (tree) => {
+        visit(tree, { tagName: "img" }, (node) => {
+          if (node.properties.class === "typst-frame") {
+            const src = node.properties.src;
+            node.tagName = "script";
+            node.properties = {
+              "data-jsx": `<AstroImage src="${src}" alt="" format="svg" inferSize />`,
+            };
+          }
+        });
+      },
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          content: () => jsx(`<Link size={16} />`),
+          properties: {
+            ariaHidden: true,
+            tabIndex: -1,
+            className: "heading-anchor",
+          },
+        },
+      ],
+      () => rehypeTransformJsxInTypst(),
+      () => (tree) => {
+        // post process: remove the link inside citation
+        visit(tree, { tagName: "a" }, (node) => {
+          if (node.properties.role === "doc-biblioref") {
+            node.tagName = "span";
+            // make DOI links open in new tab
+            visit(node, { tagName: "a" }, (node) => {
+              node.properties.target = "_blank";
+            });
+          }
+        });
+      },
+    ],
+  },
 });
